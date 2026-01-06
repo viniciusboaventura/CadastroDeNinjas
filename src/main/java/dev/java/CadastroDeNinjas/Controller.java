c4cbf63b3bc00a8eaf60b7283c0ae2e5dc64961d
viniciusboaventura <viniciuswqa@gmail.com>
2026-01-06 09:46:34 -0300

    Criado a Controller

---
+++ src/main/java/dev/java/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.java.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota!";
+    }
+}
